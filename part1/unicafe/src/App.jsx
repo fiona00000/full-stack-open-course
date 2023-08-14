b2957e1d894a5ab6b9e68b8010f4cab15a920a67
Fiona <fionaloy000@gmail.com>
2023-08-14 16:06:06 -0400

    Commit exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,17 @@
 import { useState } from 'react'
+const Header = ({text}) => {
+  return (
+    <h2>{text}</h2>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>
+}
+
+const Statistics = ({ text, value }) => {
+  return <p>{text} {value}</p>
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,9 +19,29 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => {
+    console.log("good")
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <Header text='give feedback' />
+      <Button handleClick={handleGood} text='good' />
+      <Button handleClick={handleNeutral} text='neutral' />
+      <Button handleClick={handleBad} text='bad' />
+      <Header text='statistics' />
+      <Statistics text='good' value={good} />
+      <Statistics text='neutral' value={neutral} />
+      <Statistics text='bad' value={bad} />
     </div>
   )
 }
