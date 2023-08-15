c23e159d522e731821e330adee056b524624638d
Fiona <fionaloy000@gmail.com>
2023-08-15 18:54:05 -0400

    Commit exercise 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+const Person = ({ name }) => {
+  return (
+    <p>{name}</p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: 1 }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    // console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      
+      {persons.map(person =>
+        ( <Person key={person.id} name={person.name} /> )
+      )}
+      
+    </div>
+  )
+}
+
+export default App
