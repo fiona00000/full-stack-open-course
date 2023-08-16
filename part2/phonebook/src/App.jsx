e2636567a0dd51ef6173a835ad3bc4921e2a7150
Fiona <fionaloy000@gmail.com>
2023-08-15 23:34:19 -0400

    Commit exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,10 +8,14 @@ const Person = ({ person }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567', id: 1 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -19,6 +23,10 @@ const App = () => {
 
   const  handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -39,10 +47,21 @@ const App = () => {
       setNewNumber('')
     }
   }
-
+  
+  const displayPhonebook = search
+  ? persons.filter(person => {return person.name.toUpperCase().includes(search.toUpperCase())})
+  : persons
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with <input value={search} onChange={handleSearch} />
+        </div>
+      </form>
+
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -53,8 +72,8 @@ const App = () => {
         </div>
       </form>
 
-      <h2>Numbers</h2>      
-      {persons.map(person =>
+      <h2>Numbers</h2>        
+      {displayPhonebook.map(person =>
         ( <Person key={person.id} person={person} /> )
       )}
       
