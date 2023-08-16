480044bf18e797bd5c7e8b4134b6fa51a4f0ebd1
Fiona <fionaloy000@gmail.com>
2023-08-16 19:33:06 -0400

    Commit exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({search, handleSearch}) => {
   return (
@@ -41,12 +42,18 @@ const Persons = ({ displayPhonebook }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    console.log('Effect starting...')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('Promise fulfilled')
+        setPersons(response.data)
+      })
+  },[])
+  
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
@@ -81,6 +88,8 @@ const App = () => {
       setNewNumber('')
     }
   }
+
+  
   
   const displayPhonebook = search
   ? persons.filter(person => {return person.name.toUpperCase().includes(search.toUpperCase())})
--- part2/phonebook/src/main.jsx
+++ part2/phonebook/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import axios from 'axios'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+axios.get('http://localhost:3001/persons')
+  .then(response => {
+    const persons = response.data
+    ReactDOM.createRoot(document.getElementById('root'))
+    .render(<App persons={persons}/>)
+})
