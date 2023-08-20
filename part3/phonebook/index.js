818446357dd23667c2b4cfee28fcd2806bf5a65c
Fiona <fionaloy000@gmail.com>
2023-08-20 18:47:59 -0400

    Commit exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,14 @@ const morgan = require('morgan')
 
 const app = express()
 
-app.use(morgan('tiny'))
+morgan.token('req-body', (req, res) => {
+    if (req.method === 'POST')
+        return JSON.stringify(req.body)
+    return ' '
+})
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
 let persons = [
     {
