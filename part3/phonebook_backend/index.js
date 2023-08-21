1fdbf637c7da36a858ec777da6a392fdca40130a
Fiona <fionaloy000@gmail.com>
2023-08-21 15:13:43 -0400

    Commit exercise 3.15, 16

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -9,6 +9,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.static('dist'))
+app.use(requestLogger)
 
 morgan.token('req-body', (req, res) => {
     if (req.method === 'POST')
@@ -22,6 +23,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :r
 
 app.use(express.json())
 
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
 })
@@ -32,10 +34,6 @@ app.get('/api/persons', (request, response) => {
             response.json(persons)
         })
 })
-
-const generateId = () => {
-    return Math.floor(Math.random() * 10000)
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -70,21 +68,54 @@ app.post('/api/persons', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     Phonebook.findById(request.params.id)
         .then(person => {
-            response.json(person)
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(err => {
+            next(err)
         })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Phonebook.findByIdAndRemove(request.params.id)
+        .then(person => {
+            if (person) {
+                response.status(204).end()
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            response.status(400).send({ error: 'malformatted id' })
+        })
 })
 
 app.get('/info', (request, response) => {
     const str = `<p>Phonebook has info for ${persons.length} people</p> <p>${new Date()}</p>`
     response.send(str)
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404)
+        .send({ error: "unknown endpoint" })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400)
+            .send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT)
