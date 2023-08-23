7f6521c478b0c03e03a89b2e85f413a1cf8bc854
Fiona <fionaloy000@gmail.com>
2023-08-23 11:57:27 -0400

    Commit exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, currentObject) => sum + currentObject.likes, 0)
+}
+
 module.exports = {
-    dummy
+    totalLikes
 }
