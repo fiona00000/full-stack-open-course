6495c723411d03e86e26445c731c8a09bd4f01f7
Fiona <fionaloy000@gmail.com>
2023-08-24 09:54:23 -0400

    Commit exercise 4.9

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,22 +1,26 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    }
 })
 
 module.exports = blogsRouter
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,21 +1,39 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    for (let blog of helper.initialBlogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+    }
+})
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
 }, 100000)
 
-test('notes are returned as correct amount of blog posts (6) in the JSON format', async () => {
+test('blogs are returned as correct amount of blog posts (6) in the JSON format', async () => {
     const response = await api.get('/api/blogs')
         .expect('Content-Type', /application\/json/)
-    expect(response.body).toHaveLength(6)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
 }, 100000)
+
+test('unique identifier property of the blog posts is named id',
+    async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].id).toBeDefined()
+    }
+    , 100000)
 
 
 afterAll(() => {
