4fc72563dc8d71b47b269157fa8ceb234725567b
Fiona <fionaloy000@gmail.com>
2023-08-29 18:53:41 -0400

    Commit exercise 4.16

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -10,6 +10,10 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({
       error: 'username must be unique'
     })
+  } else if (password.length < 3) {
+    return response.status(400).json({
+      error: 'password must be longer than 3 characters'
+    })
   }
 
   const saltRounds = 10
--- part4/bloglist/models/user.js
+++ part4/bloglist/models/user.js
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        minLength: 3
     },
     name: String,
     passwordHash: String,
