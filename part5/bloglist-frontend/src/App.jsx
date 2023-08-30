e75bd2887f3e990ae26689f0685dbd6192e7090e
Fiona <fionaloy000@gmail.com>
2023-08-30 14:56:40 -0400

    Commit exercise 5.1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -36,25 +36,20 @@ const App = () => {
     }
   }
 
-  if (user === null) {
-    return (
-    <div>
-      <LoginForm username={username} password={password} setUsername={setUsername} setPassword={setPassword} handleLogin={handleLogin}/>
-    </div>
-  )
-  }
-
   return (
     <div>
-      <h2>blogs</h2>
-      {user} logged in
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null ?
+        <LoginForm username={username} password={password} setUsername={setUsername} setPassword={setPassword} handleLogin={handleLogin} />
+        :
+        <div>
+          <h2>blogs</h2>
+            <p>{user.name} logged in</p>
+            {blogs.map(blog =>
+              <Blog key={blog.id} blog={blog} />
+            )}
+        </div>}      
     </div>
   )
-
-  
 }
 
 export default App
