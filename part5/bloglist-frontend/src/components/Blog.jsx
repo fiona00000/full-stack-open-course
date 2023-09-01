acd8cf046f011e87b4b1b51f1dc71e6164579ce8
Fiona Leung <fiona718@hotmail.com>
2023-09-01 18:51:56 -0400

    Commit exercise 5.8

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import blogService from '../services/blogs'
 
 const Blog = (props) => {
 
@@ -13,13 +14,24 @@ const Blog = (props) => {
   const [visible, setVisible] = useState(false)
   const show = { display: visible ? '' : 'none' }
 
+  const [likes, setLikes] = useState(props.blog.likes)
+  const updateLike = () => {
+    console.log("num like: ", props.blog.likes)
+    const updatedBlog = {
+      ...props.blog,
+      likes: props.blog.likes += 1
+    }
+    blogService.update(props.blog.id, updatedBlog)
+    setLikes(likes+1)
+  }
+
   return (
     <div style={blogStyle}>
     {props.blog.title} by {props.blog.author}
       <button onClick={() => setVisible(!visible)}>{visible ? "hide" : "view"}</button>
       <div style={show}>
         {props.blog.url}<br />
-        likes {props.blog.likes} <button>like</button><br />
+        likes {likes} <button onClick={updateLike}>like</button><br />
         {props.blog.user.name}<br />
       </div>
   </div> ) 
